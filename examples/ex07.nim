89fcbbf7ce0b5621ad4610d7f06b0172d72fc8e9
mantielero <josemaria.alkala@gmail.com>
2024-03-15 19:47:00 +0100

    ex07 added

--- examples/ex07.nim
+++ examples/ex07.nim
@@ -1,35 +1,53 @@
 # https://tesseract-ocr.github.io/tessdoc/APIExample.html
-
+# NOT WORKING
 #[
 Example to get confidence for alternative symbol choices per character for LSTM
-#include <tesseract/baseapi.h>
-#include <leptonica/allheaders.h>
-int main()
-{
-    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
-// Initialize tesseract-ocr with English, without specifying tessdata path
-    if (api->Init(NULL, "eng")) {
-        fprintf(stderr, "Could not initialize tesseract.\n");
-        exit(1);
-    }
-// Open input image with leptonica library
-  Pix *image = pixRead("/home/ubuntu/tesseract/test/testing/trainingital.tif");
-  api->SetImage(image);
-// Set lstm_choice_mode to alternative symbol choices per character, bbox is at word level.
-  api->SetVariable("lstm_choice_mode", "2");
-  api->Recognize(0);
-  tesseract::PageIteratorLevel level = tesseract::RIL_WORD;
-  tesseract::ResultIterator* res_it = api->GetIterator();
-// Get confidence level for alternative symbol choices. Code is based on
-// https://github.com/tesseract-ocr/tesseract/blob/a7a729f6c315e751764b72ea945da961638effc5/src/api/hocrrenderer.cpp#L325-L344
+
   std::vector<std::vector<std::pair<const char*, float>>>* choiceMap = nullptr;
-  if (res_it != 0) {
-    do {
-      const char* word;
-      float conf;
-      int x1, y1, x2, y2, tcnt = 1, gcnt = 1, wcnt = 0;
-     res_it->BoundingBox(level, &x1, &y1, &x2, &y2);
-     choiceMap = res_it->GetBestLSTMSymbolChoices();
+
+
+
+]#
+
+
+import tesseract
+import leptonica
+
+proc main =
+  var api = newTesseract("eng")
+  let image = pixRead("trainingital.tif")
+  api.setImage(image)
+
+  # Set lstm_choice_mode to alternative symbol choices per character, bbox is at word level.
+  var ok = api.handle.tessBaseAPISetVariable("lstm_choice_mode", "2")
+  var res = api.handle.tessBaseAPIRecognize(nil)
+
+  var ri = api.handle.tessBaseAPIGetIterator()
+  var pi = ri.tessResultIteratorGetPageIterator()
+
+  var level = RIL_WORD
+
+  # Get confidence level for alternative symbol choices. Code is based on
+  # https://github.com/tesseract-ocr/tesseract/blob/a7a729f6c315e751764b72ea945da961638effc5/src/api/hocrrenderer.cpp#L325-L344
+
+  var flag = true
+  while flag:
+    var word = ri.tessResultIteratorGetUTF8Text(level)
+    var conf = ri.tessResultIteratorConfidence(level)
+    var x1,y1,x2,y2:cint
+    var tmp = pi.tessPAgeIteratorBoundingBox(level, x1.unsafeAddr,y1.unsafeAddr, x2.unsafeAddr,y2.unsafeAddr)
+
+    var 
+      tcnt = 1
+      gcnt = 1
+      wcnt = 0
+
+    var choiceMap = ri.GetBestLSTMSymbolChoices()
+
+
+    for i in choiceMap:
+
+#[
       for (auto timestep : *choiceMap) {
         if (timestep.size() > 0) {
           for (auto & j : timestep) {
@@ -44,13 +62,8 @@ int main()
       wcnt++;
       printf("\n");
       }
-    } while (res_it->Next(level));
-  }
-  // Destroy used object and release memory
-  api->End();
-  delete api;
-  pixDestroy(&image);
-  return 0;
-}
-
 ]#
+
+    flag =  ri.tessResultIteratorNext(level)
+
+main()
