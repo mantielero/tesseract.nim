997cdc6817f44ef9465867e2ea900d6a5e13354b
mantielero <josemaria.alkala@gmail.com>
2024-03-15 19:46:50 +0100

    ex06 added

--- examples/ex06.nim
+++ examples/ex06.nim
@@ -40,14 +40,6 @@ proc main =
           echo choice, " conf: ", ci.tessChoiceIteratorConfidence
           indent = true
 
-
-    # var conf = ri.tessResultIteratorConfidence(level)
-    # var x1,y1,x2,y2:cint
-    # var tmp = pi.tessPageIteratorBoundingBox(level, x1.unsafeAddr,y1.unsafeAddr, x2.unsafeAddr,y2.unsafeAddr)
-
-    # echo "word: ", word
-    # echo "  conf: ", conf
-    # echo "  bb: ", x1, " ", y1, " ", x2, " ", y2
     flag =  pi.tessPageIteratorNext(level)
 
 main()
