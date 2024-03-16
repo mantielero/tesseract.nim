e05c677980badbf8f86a144cfdad73c429342208
mantielero <josemaria.alkala@gmail.com>
2024-03-16 13:00:29 +0100

    cleaning

--- examples/ex01.nim
+++ examples/ex01.nim
@@ -4,11 +4,15 @@ import pixie
 proc main =
   echo tessVersion()
   var tesseract = newTesseract("eng")
+
+  # Using pixie
   let image = readImage("/home/jose/docs/scanned/salida.png")
   image.rotate90()
   image.rotate90()
   image.rotate90()
+
   tesseract.setImage(image)
   tesseract.setPPI(150)
   echo tesseract.getText()
+
 main()
