1b928dca64558f98a94860218eb3b192fc20403f
mantielero <josemaria.alkala@gmail.com>
2024-03-16 13:00:42 +0100

    cleaning

--- examples/ex02.nim
+++ examples/ex02.nim
@@ -4,50 +4,15 @@ import leptonica
 
 proc main =
   var api = newTesseract("eng")
+
+  # Using leptonica
   let image = pixRead("/home/jose/docs/scanned/salida.png")
-  #api.setImage(image)
   var image270 = pixRotate90(image, -1)
 
-  # image.rotate90()
-  # image.rotate90()
-  # image.rotate90()
   api.setImage(image270)
   api.setPPI(150)
-  echo api.getText()
+  echo api.getText() # Get OCR
 
   #image.pixDestroy()
   #image270.pixDestroy()
 main()
-
-
-#[
-#include <tesseract/baseapi.h>
-#include <leptonica/allheaders.h>
-
-int main()
-{
-    char *outText;
-
-    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
-    // Initialize tesseract-ocr with English, without specifying tessdata path
-    if (api->Init(NULL, "eng")) {
-        fprintf(stderr, "Could not initialize tesseract.\n");
-        exit(1);
-    }
-
-    // Open input image with leptonica library
-    Pix *image = pixRead("/usr/src/tesseract/testing/phototest.tif");
-    api->SetImage(image);
-    // Get OCR result
-    outText = api->GetUTF8Text();
-    printf("OCR output:\n%s", outText);
-
-    // Destroy used object and release memory
-    api->End();
-    delete api;
-    delete [] outText;
-    pixDestroy(&image);
-
-    return 0;
-}
-]#
